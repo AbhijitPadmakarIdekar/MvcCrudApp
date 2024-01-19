6e819ce04a10800c947b2ac6d3a386964d3eae95
Aslam Shaikh <beingaslam@outlook.in>
2024-01-20 04:42:48 +0530

    Added Few More Files

--- MvcCrudApp.Web/Controllers/SearchController.cs
+++ MvcCrudApp.Web/Controllers/SearchController.cs
@@ -1,8 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Domain.Repository;
 
 namespace MvcCrudApp.Web.Controllers
 {
     public class SearchController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SearchController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult SearchEntry()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SearchPage()
+        {
+            return View();
+        }
     }
 }
